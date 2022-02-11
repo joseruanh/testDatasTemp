e486f8a29833a7211aa5d0217ca583e438d769d5
joseruanh <joseruanh2@gmail.com>
2022-02-11 14:42:12 -0200

    realizando mudanças

---
+++ archives/ava.js
@@ -0,0 +1,88 @@
+function refresh(){
+	location.reload();
+}
+
+
+
+
+function forum(){
+	var area = document.getElementById("area")
+
+	area.innerHTML = `
+	<h2>Datas dos fóruns:</h2>
+	<br><strong>Geografia Econômica</strong>: 23/08
+	<br><strong>Geografia da População</strong>: 20/09
+	<br><strong>Geografia Agrária</strong>: 18/10
+	<br><strong>Climatologia e Meteorologia</strong>: 15/11
+	<br><strong>Ed - Competências Sócioemocionais</strong>: n/a
+	`
+
+}
+
+function av(){
+	var area = document.getElementById("area")
+
+	area.innerHTML = `
+	<h2>Datas das avaliações virtuais:</h2>
+	<br><strong>Geografia Econômica</strong>: 30/08 | 06/09
+	<br><strong>Geografia da População</strong>: 27/09 | 04/10
+	<br><strong>Geografia Agrária</strong>: 15/10 | 01/11
+	<br><strong>Climatologia e Meteorologia</strong>: 22/11 | 29/11
+	<br><strong>Ed - Competências Sócioemocionais</strong>: 11/12 | 11/12
+	`
+}
+
+function aa(){
+		var area = document.getElementById("area")
+
+	area.innerHTML = `
+	<h2>Datas das atividades de aprendizagem:</h2>
+	<br><strong>Geografia Econômica</strong>: 27/11
+	<br><strong>Geografia da População</strong>: 27/11
+	<br><strong>Geografia Agrária</strong>: 27/11
+	<br><strong>Climatologia e Meteorologia</strong>: 27/11
+	<br><strong>Ed - Competências Sócioemocionais</strong>: n/a
+	`
+}
+
+function ad(){
+		var area = document.getElementById("area")
+
+	area.innerHTML = `
+	<h2>Datas das atividades diagnósticas:</h2>
+	<br><strong>Geografia Econômica</strong>: 27/11
+	<br><strong>Geografia da População</strong>: 27/11
+	<br><strong>Geografia Agrária</strong>: 27/11
+	<br><strong>Climatologia e Meteorologia</strong>: 27/11
+	<br><strong>Ed - Competências Sócioemocionais</strong>: n/a
+	`
+}
+
+function prova(){
+	var area = document.getElementById("area")
+
+	area.innerHTML = `
+	<h2>Datas das provas:</h2>
+	<br><strong>Geografia Econômica</strong>: 04/09
+	<br><strong>Geografia da População</strong>: 02/10
+	<br><strong>Geografia Agrária</strong>: 30/10
+	<br><strong>Climatologia e Meteorologia</strong>: 27/11
+	<br><strong>Ed - Competências Sócioemocionais</strong>: n/a
+	`
+}
+function estagio(){
+	var area = document.getElementById("area")
+
+	area.innerHTML = `
+	<h2>Data do estágio:</h2>
+	<br><strong>Portfólio</strong>: 23/10
+	`
+}
+function inter(){
+	var area = document.getElementById("area")
+
+	area.innerHTML = `
+	<h2>Data do TCP:</h2>
+	<br><strong>Portfólio</strong>: 30/10
+	`
+}
